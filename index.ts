a47483ceea00eb2ad9e3eb5f01a4950782bad86d
thadeu <guilherme.thadeu@qodeless.io>
2024-02-25 13:20:38 -0300

    adding routes

--- index.ts
+++ index.ts
@@ -3,8 +3,19 @@ import figlet from 'figlet'
 const server = Bun.serve({
   port: 3000,
   fetch(req) {
-    const body = figlet.textSync("Thadeu")
+    const url = new URL(req.url)
+
+    if(url.pathname === '/') {
+      const body = figlet.textSync("Hello!")
     return new Response(body)
+    }
+    if(url.pathname === '/about') {
+    return new Response("About me!")
+    }
+    if(url.pathname === '/contact') {
+    return new Response("Contact Us!")
+    }
+    return new Response('404!')
   }
 })
 
